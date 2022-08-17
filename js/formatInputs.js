a978512758541e64bfe1ddb731adf380c2d8b2a9
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2022-08-17 19:48:33 +0300

    Добавил отображение и рассчет суммы кредита

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -1,10 +1,14 @@
 
+import { priceFormatter } from "./formatters.js"; 
+
 //Инпуты
 
 const inputCost = document.querySelector('#input-cost');
 const inputDownPayment = document.querySelector('#input-downpayment');
 const inputTern = document.querySelector('#input-tern');
 
+const form = document.querySelector('#form');
+const totalCost = document.querySelector('#total-cost');
 
 //Cleave опции форматирования
 const cleavePriceSettings = {
@@ -18,5 +22,20 @@ const cleaveCost = new Cleave('#input-cost', cleavePriceSettings);
 
 const cleaveDownPayment = new Cleave('#input-downpayment', cleavePriceSettings);
 
+// Отображение и расчет суммы кредита
+
+calcMortgage();
+
+form.addEventListener('input', function() {
+
+    calcMortgage();
+})
+
+function calcMortgage() {
+    
+    const totalAmount = +cleaveCost.getRawValue() - cleaveDownPayment.getRawValue();
+
+    totalCost.innerText = priceFormatter.format(totalAmount);
+}
 
 
--- js/formatters.js
+++ js/formatters.js
@@ -1,2 +1,4 @@
 
 export const percentFormatter = new Intl.NumberFormat('ru-RU', { style: 'percent', maximumFractionDigits: 3 });  
+
+export const priceFormatter = new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB', maximumFractionDigits: 2 });  
